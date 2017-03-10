c6b92ddb9902c733089cff1e189d1a6c4556de7a
Adam <adambrych@gmail.com>
2017-03-10 13:04:06 +0100

    bank historia raport

--- src/pl/put/poznan/bank/Bank.java
+++ src/pl/put/poznan/bank/Bank.java
@@ -1,5 +1,38 @@
 package pl.put.poznan.bank;
 
 public class Bank {
-
+	private int id;
+	public String nazwa;
+	private Raport raport;
+	private Historia historia;
+	public Bank(String nazwa, int id){
+		this.nazwa = nazwa;
+		this.id = id;
+		this.raport = new Raport();
+		this.historia = new Historia();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNazwa() {
+		return nazwa;
+	}
+	public void setNazwa(String nazwa) {
+		this.nazwa = nazwa;
+	}
+	public Raport getRaport() {
+		return raport;
+	}
+	public void setRaport(Raport raport) {
+		this.raport = raport;
+	}
+	public Historia getHistoria() {
+		return historia;
+	}
+	public void setHistoria(Historia historia) {
+		this.historia = historia;
+	}
 }
