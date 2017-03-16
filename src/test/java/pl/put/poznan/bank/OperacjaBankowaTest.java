22fc57cf8c5f2a055a0fd013909497804febb11a
Marcin Halczuk <halczukmarcin@gmail.com>
2017-03-16 23:06:05 +0100

    Add basic tests skeleton

---
+++ src/test/java/pl/put/poznan/bank/OperacjaBankowaTest.java
@@ -0,0 +1,48 @@
+package pl.put.poznan.bank;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Cinek on 2017-03-16.
+ */
+public class OperacjaBankowaTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testWplata() throws Exception {
+        assertEquals("Wynik wplaty: ", 100, 100);
+    }
+
+    @Test
+    public void testWyplata() throws Exception {
+
+    }
+
+    @Test
+    public void testPrzelew() throws Exception {
+
+    }
+
+    @Test
+    public void testWplataOdsetek() throws Exception {
+
+    }
+
+    @Test
+    public void testNaliczOdsetki() throws Exception {
+
+    }
+}
