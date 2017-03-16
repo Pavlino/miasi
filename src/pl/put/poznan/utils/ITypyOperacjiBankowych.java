7bbb0a2708b0ee15c21a3da72820414c71dcb347
Marcin Halczuk <halczukmarcin@gmail.com>
2017-03-16 16:45:33 +0100

    Add bank operations types

---
+++ src/pl/put/poznan/utils/ITypyOperacjiBankowych.java
@@ -0,0 +1,10 @@
+package pl.put.poznan.utils;
+
+/**
+ * Created by Cinek on 2017-03-15.
+ */
+public interface ITypyOperacjiBankowych {
+    int WPLATA = 0;
+    int WYPLATA = 1;
+    int PRZELEW = 2;
+}
