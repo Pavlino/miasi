c97dbf1033105330bfff998ae0ab4b70734bc1bb
test1 <test1>
2017-04-07 13:12:46 +0200

    add flag to operation class

--- src/main/java/pl/put/poznan/bank/Wplata.java
+++ src/main/java/pl/put/poznan/bank/Wplata.java
@@ -9,19 +9,22 @@ public class Wplata implements IOperacjaBankowa {
     private ProduktBankowy konto;
     private double kwota;
     private GregorianCalendar data;
+    private boolean wykonana;
 
     public Wplata(ProduktBankowy konto, double kwota) {
         this.konto = konto;
         this.kwota = kwota;
         data = new GregorianCalendar();
+        wykonana = false;
     }
 
     public void wykonaj() throws InvalidInputException {
-        if (konto != null && kwota > 0) {
+        if (konto != null && kwota > 0 && !wykonana) {
             double stanSrodkow = konto.getSrodki();
             stanSrodkow += kwota;
             konto.setSrodki(stanSrodkow);
             dodajDoHistorii();
+            wykonana = true;
         } else {
             throw new InvalidInputException("Konto nie istnieje lub podana kwota jest ujemna");
         }
